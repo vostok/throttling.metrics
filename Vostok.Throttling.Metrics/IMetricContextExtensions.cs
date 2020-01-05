c776bda1b35305a0ef3879c977166dc825c79e10
iloktionov <iloktionov@skbkontur.ru>
2020-01-05 22:14:58 +0500

    Dummy signature and TODOs for future extension.

---
+++ Vostok.Throttling.Metrics/IMetricContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Throttling.Metrics
+{
+    [PublicAPI]
+    public static class IMetricContextExtensions
+    {
+        [NotNull]
+        public static IDisposable CreateThrottlingMetrics([NotNull] IThrottlingProvider provider)
+        {
+            // TODO(iloktionov): capacity limit
+            // TODO(iloktionov): queue limit
+            // TODO(iloktionov): max consumed capacity
+            // TODO(iloktionov): max capacity utilization
+            // TODO(iloktionov): max queue size
+            // TODO(iloktionov): max queue utilization
+            // TODO(iloktionov): max consumption by property by value
+            // TODO(iloktionov): counter by rejection reason
+            // TODO(iloktionov): summary by wait time
+
+            throw new NotImplementedException();
+        }
+    }
+}
