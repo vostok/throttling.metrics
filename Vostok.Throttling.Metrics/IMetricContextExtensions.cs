24db2d19192073527e6a422eb9fd3f1cd774dddb
iloktionov <iloktionov@skbkontur.ru>
2020-01-06 22:32:25 +0500

    Added xml-doc and built in thematic tag for metric context.

--- Vostok.Throttling.Metrics/IMetricContextExtensions.cs
+++ Vostok.Throttling.Metrics/IMetricContextExtensions.cs
@@ -7,11 +7,27 @@ namespace Vostok.Throttling.Metrics
     [PublicAPI]
     public static class IMetricContextExtensions
     {
+        /// <summary>
+        /// <para>Registers and tracks following metrics about given <see cref="IThrottlingProvider"/> instance:</para>
+        /// <list type="bullet">
+        ///     <item><description>Max capacity limit.</description></item>
+        ///     <item><description>Max capacity consumption (total).</description></item>
+        ///     <item><description>Max capacity consumption (per property). See <see cref="ThrottlingMetricsOptions"/> for more details.</description></item>
+        ///     <item><description>Max capacity utilization.</description></item>
+        ///     <item><description>Max queue limit.</description></item>
+        ///     <item><description>Max queue size.</description></item>
+        ///     <item><description>Max queue utilization.</description></item>
+        ///     <item><description>Rejection status counters.</description></item>
+        ///     <item><description>Queue wait time timings.</description></item>
+        /// </list>
+        /// <para>Adds a '<c>component = throttling</c>' tag to the provided metric <paramref name="context"/> before registering metrics.</para>
+        /// <para>Dispose of the returned result to stop metrics collection.</para>
+        /// </summary>
         [NotNull]
         public static IDisposable CreateThrottlingMetrics(
             [NotNull] this IMetricContext context,
             [NotNull] IThrottlingProvider provider,
             [CanBeNull] ThrottlingMetricsOptions options = null)
-            => new ThrottlingMetrics(provider, context, options ?? new ThrottlingMetricsOptions());
+            => new ThrottlingMetrics(provider, context.WithTag("component", "throttling"), options ?? new ThrottlingMetricsOptions());
     }
 }
